6f90f3a9c60e64de6dd0eb76fae24700208fea03
Pravesh <praveshjha5@gmail.com>
2024-07-12 02:31:07 +0530

    added shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@/components/ui/button"
 
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
